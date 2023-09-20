8ecac0599b62f8dea41d46feb62e9da40447615d
Andreas <andreas.landerer@vontobel.com>
2023-09-20 10:37:24 +0200

    Stabilized API

--- prompt.js
+++ prompt.js
@@ -6,7 +6,7 @@ const systemPrompt = `
     {text}
     
     Target structure:
-    
+   
     {{
         "fullName": "The full name of the person as 'FirstName LastName'",
         "dateOfBirth": "dd.mm.yyyy",
@@ -23,7 +23,8 @@ const systemPrompt = `
 
     Names should always be displayed as "Firstname Lastname"
     Feel free to fix any typos. 
-    Add an additional property "remarks" for additional information and hints that do not fit the other properties. 
+    Add an additional top-level property "remarks" for additional information and hints that do not fit the other properties. 
+    If you find any irregularities or contradictions, please add them to the "remarks" property as well.
     Your response must only contain the JSON object and nothing else!
 `
 
